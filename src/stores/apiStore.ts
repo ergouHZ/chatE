d011357511d642527454338a67ddf8f2f0a69242
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-03-20 23:18:00 +0000

    Update apiStore.ts

--- src/stores/apiStore.ts
+++ src/stores/apiStore.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios, { type AxiosInstance } from 'axios';
 import { defineStore } from 'pinia';
 
 //定义包含APIKEY的请求头，之后其他页面请求方法见底端
-const instance = axios.create({
+const instance: AxiosInstance = axios.create({
     baseURL: 'http://localhost:8080',
     timeout: 500,
-    headers: {"X-API-KEY": 'asjhi(&*(123hkjnskf(HIH>,412.oharmasnoqwrjnask1'}
+    headers: { 'Authorization': "asjhi(&*(123hkjnskf(HIH>,412.oharmasnoqwrjnask1" }
 });
 
 export const useApiStore = defineStore('api', {
@@ -14,7 +14,7 @@ export const useApiStore = defineStore('api', {
         serverApiKey: 'asjhi(&*(123hkjnskf(HIH>,412.oharmasnoqwrjnask1',
         openAiApiKey: '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', //访问chatGPT4
         anthropicApiKey: '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',//访问Claude
-        axiosInstance:instance
+        axiosInstance: instance
     }),
 })
 
