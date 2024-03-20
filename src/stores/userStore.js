cdb9a7ecc38b3ac986574ef12042480ff3ba2d9c
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-03-20 21:50:36 +0000

    Update userStore.js

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -28,9 +28,11 @@ export const useUserStore = defineStore("store", {
       }
     },
 
-    //在用户登录之后创建session,生成令牌，把令牌更新到服务器，然后从服务器获取权限
-    afterLoginForm (username){
-      this.initSession();
+    //在用户登录之后创建session,生成令牌，把令牌更新到服务器，然后从服务器获取权限和其他信息
+    async afterLoginForm (username){
+      const apiStore = useApiStore();
+      axiosInstance = apiStore.axiosInstance;
+      await axiosInstance.get('/user/session',)
     },
 
     //读取之后直接进行后台使用令牌登录
@@ -38,25 +40,25 @@ export const useUserStore = defineStore("store", {
       const apiStore = useApiStore();
       axiosInstance = apiStore.axiosInstance;
 
-      await axiosInstance
-        .post("/user/session/validate", {
-          token: this.session.token,
+      try {
+        const response = await axiosInstance.post("/user/session", {
+          token: this.session.token,  //通过令牌进行验证
           username: this.session.username,
-        })
-        .then((response) => {
-          console.log(response.data);
-          const session = {
-            username: response.data.username,
-            token: this.generateToken(), // 生成令牌
-            isLoggedIn: true,
-            permissions: response.data.permissions,
-            roles: response.data.roles,
-            expiresAt: new Date().getTime() + 36000000, // 令牌过期时间为1周
-          };
-
-          this.setSession(session); //将生成的所有的信息，保存到新的本地用户会话
-          localStorage.setItem("session", JSON.stringify(session)); // 存储到本地存储
         });
+        console.log(response.data);
+        const session = {
+          username: response.data.username,
+          token: this.generateToken(), // 生成令牌
+          isLoggedIn: true,
+          permissions: response.data.permissions,
+          roles: response.data.roles,
+          expiresAt: new Date().getTime() + 36000000, // 令牌过期时间为1周
+        };
+        this.setSession(session); // 将生成的所有的信息，保存到新的本地用户会话
+        localStorage.setItem("session", JSON.stringify(session)); // 存储到本地存储
+      } catch (error) {
+        console.log(error);
+      }
     },
 
     logout() {
