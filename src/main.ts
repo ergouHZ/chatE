ce7ca977a33b6ed96fb8c82d6da8368e968249bb
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-05-13 21:33:55 +0100

    Update main.ts
    消除主页奇怪的边框

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,10 @@ import App from './App.vue'
 import router from './router'
 // 在 main.js 或其他 Vue 入口文件中引入 markdown-it 库
 import MarkdownIt from 'markdown-it'
-
 // 创建一个 Markdown 解析器实例
 const md = new MarkdownIt();
 
-
 const app = createApp(App)
-
 // 创建一个 Vue 自定义指令
 app.directive('markdown', {
     // 当绑定元素插入到 DOM 中时执行
@@ -32,7 +29,21 @@ app.use(router)
 app.mount('#app')
 
 //axios配置
-//axios.defaults.baseURL = 'http://localhost:8080';
-axios.defaults.baseURL = 'https://chatserver.eeeegou.com';
+axios.defaults.baseURL = 'http://localhost:8080';
+//axios.defaults.baseURL = 'https://chatserver.eeeegou.com';
 axios.defaults.withCredentials=true;  //携带cookie
 /* axios.defaults.headers.common['authorization'] = 'aoiodiqjono1n24oi15on6o1n62ion62n4jk1n4j1n5k32naogoani2on5i2'; */
+
+
+const style = document.createElement('style');
+style.innerHTML = `
+  html, body, #app {
+    margin: 0;
+    padding: 0;
+    border: none; /* 确保这些元素没有边框 */
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+  }
+`;
+document.head.appendChild(style);
